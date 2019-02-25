9235c39afeac9c612b45326685f6dce13100ece5
EC2 Default User <ec2-user@ip-172-31-72-237.ec2.internal>
2019-02-25 09:37:33 +0000

    rakuten api book search

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,45 @@
+class ItemsController < ApplicationController
+  def new
+    @items = []
+
+    @title = params[:title]
+    if @title.present?
+      results = RakutenWebService::Books::Book.search({
+        title: @title,
+        hits: 30,
+      })
+
+      results.each do |result|
+        item = Item.new(read(result))
+        @items << item
+      end
+    end
+  end
+  
+   def create
+    @item = Item.find_or_initialize_by(isbn: params[:isbn])
+
+    unless @item.persisted?
+      results = RakutenWebService::Books::Book.search(isbn: @item.isbn)
+      @item = Item.new(read(results.first))
+      @item.save
+    end
+   end
+
+private
+
+  def read(result)
+    title = result['title']
+    url = result['itemUrl']
+    isbn = result['isbn']
+    image_url = result['mediumImageUrl'].gsub('?_ex=120x120', '')
+
+    {
+      title: title,
+      url: url,
+      isbn: isbn,
+      image_url: image_url,
+    }
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190224090823) do
+ActiveRecord::Schema.define(version: 20190225084558) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.string   "isbn"
+    t.string   "url"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
